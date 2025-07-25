660380eaedb72113d8f9f73b36dbea2da6c7f892
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-25 15:02:30 +0800

    更改檔案路徑

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './normalize.css'
 import './App.css'
-import Navbar from './sections/Navbar/Navbar'
-import Brand from './sections/Brand/Brand'
-import Service from './sections/Service/Service'
-import Operation from './sections/Operation/Operation'
-import Evaluate from './sections/Evaluate/Evaluate'
-import Contact from './sections/Contact/Contact'
-import Footer from './sections/Footer/Footer'
+import './variable.css'
+import Navbar from './sections/1_Navbar/Navbar'
+import Brand from './sections/2_Brand/Brand'
+import Service from './sections/3_Service/Service'
+import Operation from './sections/4_Operation/Operation'
+import Evaluate from './sections/5_Evaluate/Evaluate'
+import Contact from './sections/6_Contact/Contact'
+import Footer from './sections/7_Footer/Footer'
 
 
 function App() {
